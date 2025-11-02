b964438fc22dae2b430e056377b94c33dfabbffd
wonpark1 <2023111927@dgu.ac.kr>
2025-11-03 00:41:08 +0900

    feat: popularSlide contentbox import

--- src/components/AI/PopularSlide/PopularSlide.jsx
+++ src/components/AI/PopularSlide/PopularSlide.jsx
@@ -4,6 +4,7 @@ import {
   EmojiPanelWrapper,
 } from "./PopularSlide.styles";
 import EmojiPanel from "../../Audience/EmojiPanel";
+import ContentBox from "../ContentBox/ContentBox";
 
 const PopularSlide = () => {
   return (
@@ -12,6 +13,13 @@ const PopularSlide = () => {
       <EmojiPanelWrapper>
         <EmojiPanel />
       </EmojiPanelWrapper>
+      <ContentBox
+        title="재미있는 반응을 가장 많이 받은 슬라이드"
+        content="이모지별 인기 슬라이드 내용"
+        variant="text"
+        width="auto"
+        height="auto"
+      />
     </PopularSlideContainer>
   );
 };
